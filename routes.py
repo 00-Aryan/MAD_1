a491b21811c51ed12d396134e2f3c1a1d9daa38c
00-Aryan <22f2000697@ds.study.iitm.ac.in>
2024-11-17 10:18:59 +0530

    working on registration page login page is created

--- routes.py
+++ routes.py
@@ -11,3 +11,7 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('Registration.html')
